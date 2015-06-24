75c9f2c8f52b7b94a423531c547517cad7d69dcb
Irina Belyaeva <ibelyaev@jcvi.org>
2015-06-24 10:32:15 -0400

    Web- application context test

--- webapp/src/org/thalemine/web/utils/WebApplicationContextLocator.java
+++ webapp/src/org/thalemine/web/utils/WebApplicationContextLocator.java
@@ -3,17 +3,17 @@ package org.thalemine.web.utils;
 import java.util.Properties;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+import org.intermine.web.logic.PortalHelper;
 
 import org.intermine.web.logic.session.SessionMethods;
 import org.intermine.web.util.URLGenerator;
 
 public class WebApplicationContextLocator {
 	
-	//private static String SERVICE_URL;
+	private static String SERVICE_URL;
 	private static final String SERVICE_PREFIX = "/service";
 	
-	private static final String SERVICE_URL = "https://apps.araport.org/demo-thalemine/service/";
+	//private static final String SERVICE_URL = "https://apps.araport.org/demo-thalemine/service/";
 	//private static final String SERVICE_URL = "http://localhost:8081/thalemine/service/";
 	
 	//private static final String SERVICE_URL = "http://ibelyaev-lx.jcvi.org:8081/thalemine/service/";
@@ -32,10 +32,9 @@ public class WebApplicationContextLocator {
 		
 		*/
 		
-	//Properties webProperties = SessionMethods.getWebProperties(request.getSession().
-		//            getServletContext());
-
 	//SERVICE_URL = new URLGenerator(request).getPermanentBaseURL()+SERVICE_PREFIX;
+	
+	SERVICE_URL = PortalHelper.getBaseUrl(request)+SERVICE_PREFIX;
 		      
 	return SERVICE_URL;
 		
