81d4825aa8b893749c9509d3f3f2c8f3427e13aa
Irina Belyaeva <ibelyaev@jcvi.org>
2015-06-24 11:01:49 -0400

    Sandbox deployment

--- webapp/src/org/thalemine/web/utils/WebApplicationContextLocator.java
+++ webapp/src/org/thalemine/web/utils/WebApplicationContextLocator.java
@@ -10,10 +10,10 @@ import org.intermine.web.util.URLGenerator;
 
 public class WebApplicationContextLocator {
 	
-	private static String SERVICE_URL;
+	//private static String SERVICE_URL;
 	private static final String SERVICE_PREFIX = "/service";
 	
-	//private static final String SERVICE_URL = "https://apps.araport.org/demo-thalemine/service/";
+	private static final String SERVICE_URL = "https://apps.araport.org/demo-thalemine/service/";
 	//private static final String SERVICE_URL = "http://localhost:8081/thalemine/service/";
 	
 	//private static final String SERVICE_URL = "http://ibelyaev-lx.jcvi.org:8081/thalemine/service/";
@@ -34,7 +34,7 @@ public class WebApplicationContextLocator {
 		
 	//SERVICE_URL = new URLGenerator(request).getPermanentBaseURL()+SERVICE_PREFIX;
 	
-	SERVICE_URL = PortalHelper.getBaseUrl(request)+SERVICE_PREFIX;
+	//SERVICE_URL = PortalHelper.getBaseUrl(request)+SERVICE_PREFIX;
 		      
 	return SERVICE_URL;
 		
